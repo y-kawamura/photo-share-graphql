780d7a0e69179f0de4b5b0aac2ab74741a5f800d
y-kawamura <tetora.y@gmail.com>
2020-04-29 14:25:20 +0900

    Use cache for query data

--- client/src/index.js
+++ client/src/index.js
@@ -5,9 +5,22 @@ import App from './App'
 import * as serviceWorker from './serviceWorker'
 
 import { ApolloProvider } from 'react-apollo'
-import ApolloClient from 'apollo-boost'
+import ApolloClient, { InMemoryCache } from 'apollo-boost'
+import { persistCache } from 'apollo-cache-persist'
+
+const cache = new InMemoryCache()
+persistCache({
+  cache,
+  storage: localStorage
+})
+
+if (localStorage['apollo-cache-persist']) {
+  let cacheData = JSON.parse(localStorage['apollo-cache-persist'])
+  cache.restore(cacheData)
+}
 
 const client = new ApolloClient({
+  cache,
   uri: 'http://localhost:4000/graphql',
   request: (operation) => {
     operation.setContext((context) => ({
