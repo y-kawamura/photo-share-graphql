6660dbb5a22a1ac1a7713b362155bc30d5994a7f
y-kawamura <tetora.y@gmail.com>
2020-04-29 09:11:52 +0900

    Add user list

--- client/src/index.js
+++ client/src/index.js
@@ -1,32 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
 
-import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo'
+import ApolloClient from 'apollo-boost'
 
-const client = new ApolloClient({ uri: 'http://localhost:4000/' });
-
-const query = gql`
-  {
-    hello
-  }
-`;
-
-client
-  .query({ query })
-  .then(({ data }) => console.log('data', data))
-  .catch(console.error);
+const client = new ApolloClient({ uri: 'http://localhost:4000/graphql' })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
